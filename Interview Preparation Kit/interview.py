a8d1c65a32527c808907efb3b6ae266d31c79498
Nam-SH <gtsmell@gmail.com>
2020-05-21 21:20:23 +0900

    20200521 | Update: String Manipulation

--- Interview Preparation Kit/interview.py
+++ Interview Preparation Kit/interview.py
@@ -1,9 +1,36 @@
 def substrCount(n, s):
-    ans = 0
-    for i in range(n):
-        if s[i] == s[i]
+    res = n
 
-    print(ans)
+    spcCnt = 0
+    currCnt = 0
+    prevCnt = 0
+    prevPrevCnt = 0
 
-substrCount(7, "abcbaba")
+    for i in range(1, n):
+        prev = s[i - 1]
+        curr = s[i]
 
+        if prev == curr:
+            currCnt += 1
+            res += currCnt
+
+            if spcCnt > 0:
+                spcCnt -= 1
+                res += 1
+        else:
+            currCnt = 0
+            if i > 1 and s[i - 2] == curr:
+                spcCnt = prevPrevCnt
+                res += 1
+            else:
+                spcCnt = 0
+
+        if i > 1:
+            prevPrevCnt = prevCnt
+
+        prevCnt = currCnt
+
+    return res
+
+
+substrCount()
