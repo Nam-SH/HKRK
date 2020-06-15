1192c859a82e403b01105ebfd60bcc407e39c3f6
Nam-SH <gtsmell@gmail.com>
2020-06-15 16:50:35 +0900

    20200615 | Add: Graph

--- Interview Preparation Kit/interview.py
+++ Interview Preparation Kit/interview.py
@@ -1,22 +1,38 @@
-def poisonousPlants(p):
-
-    temp = []
-    Max = 0
-
-    for i in p:
-        days = 1
-        while temp and temp[-1][0] >= i:
-            val, d = temp.pop()
-            days = max(days, d + 1)
-
-        if not temp:
-            days = 0
-
-        Max = max(Max, days)
-        temp.append((i, days))
-
-    return Max
+from collections import deque
 
 
-n = 7
-poisonousPlants([6, 5, 8, 4, 7, 10, 9])
+class Graph:
+    num = 0
+
+    def __init__(self, num):
+        self.num = num
+        self.lst = [[] for _ in range(num)]
+
+    def connect(self, x, y):
+        self.lst[x] += [y]
+        self.lst[y] += [x]
+
+    def find_all_distances(self, start):
+        q = deque()
+        q.append(start)
+        visit = [-1] * self.num
+        visit[start] = 0
+        while q:
+            curr = q.popleft()
+            for i in self.lst[curr]:
+                if visit[i] == -1:
+                    visit[i] = visit[curr] + 6
+                    q.append(i)
+        print(*[val for (idx, val) in enumerate(visit) if idx != start])
+
+
+for i in range(int(input())):
+    n, m = map(int, input().split())
+    graph = Graph(n)
+
+    for i in range(m):
+        x, y = [int(x) for x in input().split()]
+        graph.connect(x - 1, y - 1)
+
+    s = int(input())
+    graph.find_all_distances(s - 1)
