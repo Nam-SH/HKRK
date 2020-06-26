a2f4c2b1e0e7868a384a646ecf809d0c15a03395
Nam-SH <gtsmell@gmail.com>
2020-06-26 23:46:40 +0900

    20200626 | Update: Graph

--- Interview Preparation Kit/interview.py
+++ Interview Preparation Kit/interview.py
@@ -1,38 +1,55 @@
-from collections import deque
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
 
 
-class Graph:
-    num = 0
+# Complete the roadsAndLibraries function below.
+def dfs(arr, idx, visit):
+    
+    if visit[idx]:
+        return 0
 
-    def __init__(self, num):
-        self.num = num
-        self.lst = [[] for _ in range(num)]
+    visit[idx] = 1
+    cnt = 1
 
-    def connect(self, x, y):
-        self.lst[x] += [y]
-        self.lst[y] += [x]
-
-    def find_all_distances(self, start):
-        q = deque()
-        q.append(start)
-        visit = [-1] * self.num
-        visit[start] = 0
-        while q:
-            curr = q.popleft()
-            for i in self.lst[curr]:
-                if visit[i] == -1:
-                    visit[i] = visit[curr] + 6
-                    q.append(i)
-        print(*[val for (idx, val) in enumerate(visit) if idx != start])
+    for v in arr[idx]:
+        cnt += dfs(arr, v, visit)
+    return cnt
 
 
-for i in range(int(input())):
-    n, m = map(int, input().split())
-    graph = Graph(n)
+def roadsAndLibraries(n, c_lib, c_road, cities):
 
-    for i in range(m):
-        x, y = [int(x) for x in input().split()]
-        graph.connect(x - 1, y - 1)
+    arr = [[] for _ in range(n + 1)]
+    visit = [0] * (n + 1)
 
-    s = int(input())
-    graph.find_all_distances(s - 1)
+    for u, v in cities:
+        arr[u] += [v]
+        arr[v] += [u]
+
+    cost = 0
+    for i in range(1, n + 1):
+        cnt = dfs(arr, i, visit)
+        if cnt:
+            cost += c_lib + (cnt - 1) * min(c_lib, c_road)
+    return cost
+
+
+q = 1
+for q_itr in range(q):
+    n = 3
+
+    m = 3
+
+    c_lib = 2
+
+    c_road = 1
+
+    cities = [[1, 2], [3, 1], [2, 3]]
+
+    result = roadsAndLibraries(n, c_lib, c_road, cities)
+
+    print(result)
