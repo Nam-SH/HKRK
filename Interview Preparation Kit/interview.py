7bc31420b91147a735db72717d94b12e2f0b4b2a
Nam-SH <gtsmell@gmail.com>
2020-07-06 20:47:40 +0900

    20200706 | Update: Linked Lists

--- Interview Preparation Kit/interview.py
+++ Interview Preparation Kit/interview.py
@@ -6,120 +6,74 @@ import random
 import re
 import sys
 
-# Complete the balancedForest function below.
-from collections import deque
+class DoublyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+        self.prev = None
 
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
 
-class Node:
-    def __init__(self, value, nid):
-        self.val = value
-        self.children = []
-        self.cost = value
-        self.nid = nid
+    def insert_node(self, node_data):
+        node = DoublyLinkedListNode(node_data)
 
-
-def constructTree(v, e):
-
-    l_v = len(v)
-    tbl = {i: [] for i in range(l_v)}
-    discovered = [None for i in range(l_v)]  # 0-based
-
-    for src, des in e:
-        src -= 1  # 1-base -> 0-base
-        des -= 1  # 1-base -> 0-base
-        tbl[src].append(des)
-        tbl[des].append(src)
-
-    discovered[0] = Node(v[0], 0)
-    stack = deque([(False, 0)])  # (type, id_zerob)
-
-    while stack:
-        flag_agg, curr_id = stack.pop()
-        if flag_agg:
-            pnode = discovered[curr_id]
-            for cnode in pnode.children:
-                pnode.cost += cnode.cost
+        if not self.head:
+            self.head = node
         else:
-            stack.append((True, curr_id))
-            for adj_id in tbl[curr_id]:
-                if discovered[adj_id] is None:
-                    tmp_newnode = discovered[adj_id] = Node(v[adj_id], adj_id)
-                    discovered[curr_id].children.append(tmp_newnode)
-                    stack.append((False, adj_id))
-    return discovered[0]
+            self.tail.next = node
+            node.prev = self.tail
 
 
-_MAX_COST = 5 * (10 ** 13) + 1
+        self.tail = node
+
+def print_doubly_linked_list(node, sep):
+    while node:
+        print(str(node.data), end=" ")
+
+        node = node.next
 
 
-def aux_traversal(root, size):
-    best = _MAX_COST
-    stack = deque([root])
-    path_table = [False for _ in range(size)]
-    path_table[0] = True
+# Complete the reverse function below.
 
-    while stack:
-        cur_node = stack.pop()
-        if type(cur_node) == int:
-            path_table[cur_node] = False
-        else:
-            path_table[cur_node.nid] = True
-            stack.append(cur_node.nid)  # Insert exit flag
-            for chd_node in cur_node.children:
-                target_root = root.cost - chd_node.cost
-                target_split = chd_node.cost
+#
+# For your reference:
+#
+# DoublyLinkedListNode:
+#     int data
+#     DoublyLinkedListNode next
+#     DoublyLinkedListNode prev
+#
+#
+def reverse(head):
+    if not head:
+        return head
 
-                res_equal = target_root if target_root == target_split else _MAX_COST
-                res_root = best_split(root, target_split, chd_node.nid, path_table)
-                res_split = best_split(chd_node, target_root)
-                best = min(best, res_equal, res_root, res_split)
-                if best == 0:
-                    return 0
-                stack.append(chd_node)
-    return best if best < _MAX_COST else -1
+    prev = None
+    curr = head
+    while curr:
+        nxt = curr.next
+        curr.prev = nxt
+        curr.next = prev
+        prev = curr
+        curr = nxt
 
+    return prev
 
-def best_split(root, target, exclude=None, path=None):
-    whole_cost = root.cost - (target if exclude is not None else 0)
+t = 1
 
-    if whole_cost > target * 2 or whole_cost <= target:
-        return _MAX_COST
+for t_itr in range(t):
+    llist_count = 4
 
-    best = _MAX_COST
-    stack = deque([root])
+    llist = DoublyLinkedList()
 
-    while stack:
-        cur_node = stack.pop()
-        for chd_node in cur_node.children:
-            if chd_node.nid != exclude:
-                sub_cost = chd_node.cost - (target if path and path[chd_node.nid] else 0)
-                root_cost = whole_cost - sub_cost
+    data = [1, 2, 3, 4]
+    for _ in range(llist_count):
+        llist_item = data[_]
+        llist.insert_node(llist_item)
 
-                if root_cost == sub_cost == target:
-                    return 0
+    llist1 = reverse(llist.head)
 
-                elif root_cost == target and sub_cost < target:
-                    best = min(best, target - sub_cost)
-                elif sub_cost == target and root_cost < target:
-                    best = min(best, target - root_cost)
-
-                stack.append(chd_node)
-    return best
-
-
-def balancedForest(tree_values, tree_edges):
-    t = constructTree(tree_values, tree_edges)
-    return aux_traversal(t, len(tree_values))
-
-
-q = 1
-
-n = 5
-
-c = [1, 2, 2, 1, 1]
-
-edges = [[1, 2], [1, 3], [3, 5], [1, 4]]
-
-result = balancedForest(c, edges)
-
-print(result)
+    print_doubly_linked_list(llist1, ' ')
